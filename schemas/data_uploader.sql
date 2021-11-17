68f4ab89af78898a21c8aad761c7f7bdf36831dc
Quinton Maki <quintonmaki@gmail.com>
2021-11-17 15:23:54 -0800

    Modified schema, began data upload

---
+++ schemas/data_uploader.sql
@@ -0,0 +1,3 @@
+\c loungeo;
+
+\COPY reviews(review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) FROM './data/reviews.csv' WITH DELIMITER ',' CSV HEADER;
--- schemas/schema.sql
+++ schemas/schema.sql
@@ -1,97 +1,103 @@
-CREATE SCHEMA loungeo_reviews;
+DROP DATABASE IF EXISTS loungeo;
+CREATE DATABASE loungeo;
+\c loungeo;
 
-CREATE TABLE "loungeo_reviews.photos" (
-	"id" serial PRIMARY KEY,
-	"url" varchar(255) DEFAULT 'null',
-	"review_id" integer NOT NULL,
-	CONSTRAINT "photos_pk" PRIMARY KEY ("id")
+DROP TABLE IF EXISTS ratings CASCADE;
+DROP TABLE IF EXISTS recommended CASCADE;
+DROP TABLE IF EXISTS characteristic_votes CASCADE;
+DROP TABLE IF EXISTS characteristics CASCADE;
+DROP TABLE IF EXISTS photos CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS product CASCADE;
+
+
+
+CREATE TABLE photos (
+	photos_id serial PRIMARY KEY,
+	url varchar(255) DEFAULT null,
+	review_id integer NOT NULL
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "loungeo_reviews.reviews" (
-	"review_id" serial PRIMARY KEY,
-	"product_id" integer NOT NULL,
-	"rating" smallint NOT NULL CHECK valid_rating (rating > 0 AND rating < 6),
-	"date" bigint NOT NULL,
-	"summary" varchar(60) NOT NULL,
-	"body" varchar(1000) NOT NULL,
-	"recommend" BOOLEAN NOT NULL DEFAULT 'false',
-	"reported" BOOLEAN NOT NULL DEFAULT 'false',
-	"reviewer_name" varchar(50) NOT NULL,
-	"reviewer_email" varchar(50) NOT NULL,
-	"response" varchar(255) DEFAULT 'null',
-	"helpfulness" integer NOT NULL DEFAULT '0',
-	CONSTRAINT "reviews_pk" PRIMARY KEY ("review_id")
+CREATE TABLE reviews (
+	review_id serial PRIMARY KEY,
+	product_id integer NOT NULL,
+	rating smallint NOT NULL CHECK (rating > 0 AND rating < 6),
+	date bigint NOT NULL,
+	summary varchar(255) NOT NULL,
+	body varchar(1000) NOT NULL,
+	recommend BOOLEAN NOT NULL DEFAULT false,
+	reported BOOLEAN NOT NULL DEFAULT false,
+	reviewer_name varchar(50) NOT NULL,
+	reviewer_email varchar(50) NOT NULL,
+	response varchar(255) DEFAULT null,
+	helpfulness integer NOT NULL DEFAULT 0
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "loungeo_reviews.product" (
-	"product_id" integer PRIMARY KEY,
-	"recommend_id" integer NOT NULL,
-	"ratings_id" integer NOT NULL
+CREATE TABLE product (
+	product_id integer PRIMARY KEY,
+	recommended_id integer NOT NULL,
+	ratings_id integer NOT NULL
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "loungeo_reviews.characteristics" (
-	"characteristic_id" serial PRIMARY KEY,
-	"characteristic_name" varchar(32) NOT NULL,
-	"product_id" integer NOT NULL,
-	"characteristic_vote_id" integer NOT NULL,
-	CONSTRAINT "characteristics_pk" PRIMARY KEY ("characteristic_id")
+CREATE TABLE characteristics (
+	characteristics_id serial PRIMARY KEY,
+	characteristic_name varchar(32) NOT NULL,
+	product_id integer NOT NULL,
+	characteristic_vote_id integer NOT NULL
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "loungeo_reviews.characteristic_votes" (
-	"characteristic_id" serial PRIMARY KEY,
-	"total_score" integer NOT NULL DEFAULT '0',
-	"total_votes" integer NOT NULL DEFAULT '0',
-	CONSTRAINT "characteristic_votes_pk" PRIMARY KEY ("characteristic_id")
+CREATE TABLE characteristic_votes (
+	characteristic_vote_id serial PRIMARY KEY,
+	total_score integer NOT NULL DEFAULT 0,
+	total_votes integer NOT NULL DEFAULT 0
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "loungeo_reviews.recommended" (
-	"recommended_id" serial PRIMARY KEY,
-	"true_vote" integer NOT NULL DEFAULT '0',
-	"false_vote" integer NOT NULL DEFAULT '0',
-	CONSTRAINT "recommended_pk" PRIMARY KEY ("id")
+CREATE TABLE recommended (
+	recommended_id serial PRIMARY KEY,
+	true_vote integer NOT NULL DEFAULT 0,
+	false_vote integer NOT NULL DEFAULT 0
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "loungeo_reviews.ratings" (
-	"ratings_id" serial PRIMARY KEY,
-	"1" integer NOT NULL DEFAULT '0',
-	"2" integer NOT NULL DEFAULT '0',
-	"3" integer NOT NULL DEFAULT '0',
-	"4" integer NOT NULL DEFAULT '0',
-	"5" integer NOT NULL DEFAULT '0',
-	CONSTRAINT "ratings_pk" PRIMARY KEY ("id")
+CREATE TABLE ratings (
+	ratings_id serial PRIMARY KEY,
+	rating_1 integer NOT NULL DEFAULT 0,
+	rating_2 integer NOT NULL DEFAULT 0,
+	rating_3 integer NOT NULL DEFAULT 0,
+	rating_4 integer NOT NULL DEFAULT 0,
+	rating_5 integer NOT NULL DEFAULT 0
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-ALTER TABLE "photos" ADD CONSTRAINT "photos_fk0" FOREIGN KEY ("review_id") REFERENCES "reviews"("review_id");
-ALTER TABLE "reviews" ADD CONSTRAINT "reviews_fk0" FOREIGN KEY ("product_id") REFERENCES "product"("product_id");
-ALTER TABLE "characteristics" ADD CONSTRAINT "characteristics_fk0" FOREIGN KEY ("product_id") REFERENCES "product"("product_id");
-ALTER TABLE "characteristic_votes" ADD CONSTRAINT "characteristic_votes_fk0" FOREIGN KEY ("characteristic_id") REFERENCES "characteristics"("characteristic_vote_id");
-ALTER TABLE "recommended" ADD CONSTRAINT "recommended_fk0" FOREIGN KEY ("id") REFERENCES "product"("recommend_id");
-ALTER TABLE "ratings" ADD CONSTRAINT "ratings_fk0" FOREIGN KEY ("id") REFERENCES "product"("ratings_id");
+ALTER TABLE photos ADD FOREIGN KEY (review_id) REFERENCES reviews(review_id);
+ALTER TABLE reviews ADD FOREIGN KEY (product_id) REFERENCES product(product_id);
+ALTER TABLE characteristics ADD FOREIGN KEY (product_id) REFERENCES product(product_id);
+ALTER TABLE characteristics ADD FOREIGN KEY (characteristic_vote_id) REFERENCES characteristic_votes(characteristic_vote_id);
+ALTER TABLE product ADD FOREIGN KEY (recommended_id) REFERENCES recommended(recommended_id);
+ALTER TABLE product ADD FOREIGN KEY (ratings_id) REFERENCES ratings(ratings_id);
